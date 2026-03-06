f68b88204eb22f02929932c31333a39e201e73c8
youngrae-debug <cho02116@gmail.com>
2026-03-06 17:15:37 +0900

    Initialize RevenueCat SDK on app startup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,19 @@
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { config } from "../gluestack-ui.config";
+import { initializeSubscriptionSDK } from "../services/subscription";
 import { JournalProvider } from "../store/journalStore";
 
 export default function RootLayout() {
+  React.useEffect(() => {
+    initializeSubscriptionSDK().catch((error) => {
+      console.warn("RevenueCat initialization failed", error);
+    });
+  }, []);
+
   return (
     <SafeAreaProvider>
       <GluestackUIProvider config={config}>
